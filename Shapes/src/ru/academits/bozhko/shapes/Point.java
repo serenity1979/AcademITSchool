03f9531108f09978bdd05ba33fdbc69a44842404
Serenity1979 <serenity1979@mail.ru>
2017-09-10 00:16:58 +0700

    Задача по лекции (отрезок)

---
+++ Shapes/src/ru/academits/bozhko/shapes/Point.java
@@ -0,0 +1,39 @@
+package ru.academits.bozhko.shapes;
+
+/**
+ * Created by Serenity on 09.09.2017.
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void move(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public double getDistance(Point p) {
+        return Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2));
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
---
+++ Shapes/src/ru/academits/bozhko/shapes/Segment.java
@@ -0,0 +1,34 @@
+package ru.academits.bozhko.shapes;
+
+/**
+ * Created by Serenity on 10.09.2017.
+ */
+public class Segment {
+    private Point begin;
+    private Point end;
+
+    public Segment(Point begin, Point end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public double getLength() {
+        return end.getDistance(begin);
+    }
+
+    public Point getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Point begin) {
+        this.begin = begin;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+    }
+}
