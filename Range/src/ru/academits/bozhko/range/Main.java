4cebd01d6f558a48e957b29e36fa5b4eaf03a410
Serenity1979 <serenity1979@mail.ru>
2017-09-10 23:31:34 +0700

    отдельный модуль (Range)

--- Range/src/ru/academits/bozhko/range/Main.java
+++ Range/src/ru/academits/bozhko/range/Main.java
@@ -5,7 +5,7 @@ package ru.academits.bozhko.range;
  */
 public class Main {
     public static void main(String[] args) {
-        Range myRange = new Range(100.5, 10.65);
+        Range myRange = new Range(10.5, 100.65);
         double selectNumber = 99.2;
         if (myRange.isInside(selectNumber)) {
             System.out.printf("число %.3f принадлежит числовому интервалу {%.3f; %.3f }", selectNumber, myRange.getFrom(), myRange.getTo());
@@ -14,21 +14,34 @@ public class Main {
         }
         System.out.printf("%nДлина числового интервала {%.3f; %.3f } = %.3f", myRange.getFrom(), myRange.getTo(), myRange.getLength());
         System.out.println();
+
         myRange.setFrom(selectNumber);
         if (myRange.isInside(selectNumber / 2)) {
-            System.out.printf("число %.3f принадлежит числовому инервалу {%.3f; %.3f }", selectNumber / 2, myRange.getFrom(), myRange.getTo());
+            System.out.printf("число %.3f принадлежит числовому интервалу {%.3f; %.3f }", selectNumber / 2, myRange.getFrom(), myRange.getTo());
         } else {
             System.out.printf("число %.3f не принадлежит числовому интервалу {%.3f; %.3f }", selectNumber / 2, myRange.getFrom(), myRange.getTo());
         }
         System.out.printf("%nДлина числового интервала {%.3f; %.3f } = %.3f", myRange.getFrom(), myRange.getTo(), myRange.getLength());
+
         myRange.setTo(250);
         System.out.println();
         if (myRange.isInside(selectNumber)) {
-            System.out.printf("число %.3f принадлежит числовому инервалу {%.3f; %.3f }", selectNumber, myRange.getFrom(), myRange.getTo());
+            System.out.printf("число %.3f принадлежит числовому интервалу {%.3f; %.3f }", selectNumber, myRange.getFrom(), myRange.getTo());
         } else {
             System.out.printf("число %.3f не принадлежит числовому интервалу {%.3f; %.3f }", selectNumber, myRange.getFrom(), myRange.getTo());
         }
-        System.out.printf("%nДлина числового интервала {%.3f; %.3f } = %.3f", myRange.getFrom(), myRange.getTo(), myRange.getLength());
+        System.out.printf("%nДлина числового интервала {%.3f; %.3f } = %.3f%n", myRange.getFrom(), myRange.getTo(), myRange.getLength());
+
+ /*       Range secondRange = new Range(50.1, 120.1);
+        Range intersectionRange = myRange.getIntersection(secondRange);
+        System.out.printf("Числовой интервал пересичения {%.3f; %.3f }%n", intersectionRange.getFrom(), intersectionRange.getTo());
+
+        Range[] merger = myRange.toUnite(secondRange);
+        System.out.print("Объединение 2 интервало. Итог:");
+        for (Range e : merger) {
+            System.out.printf(" {%.3f; %.3f } ", e.getFrom(), e.getTo());
+        }
+*/
     }
 
 }
--- Range/src/ru/academits/bozhko/range/Range.java
+++ Range/src/ru/academits/bozhko/range/Range.java
@@ -8,46 +8,82 @@ public class Range {
     private double to;
 
     public Range(double from, double to) {
-        double max = (from > to) ? from : to;
-        double min = (from < to) ? from : to;
-        this.from = min;
-        this.to = max;
+        this.from = from;
+        this.to = to;
     }
 
     public void setFrom(double from) {
-
-        double max = (from > to) ? from : to;
-        double min = (from < to) ? from : to;
-        this.from = min;
-        this.to = max;
+        this.from = from;
     }
 
     public double getFrom() {
         return this.from;
     }
 
-    //  public double[] confluance
-   /*   public double[][] merger (Range secondRange) {
-          if to >=  secondRange.from {
-              merger[0][0]= from;
-              merger[0][1]= secondRange.to;
+// todo Пересечение в доработке
+ /*
+  public Range getIntersection(Range secondRange) {
+        Range intersectionRange = new Range(0, 0);
 
-          }
-      } */
+        if ((isInside(secondRange.from)) && (isInside(secondRange.to))) {
+            return intersectionRange; // null
+        }
+
+        if (from < secondRange.from) {
+            if (isInside(secondRange.from)) {
+                intersectionRange.from = secondRange.from;
+                if (isInside(secondRange.to)) {
+                    intersectionRange.to = secondRange.to;
+                } else {
+                    intersectionRange.to = to;
+                }
+            }
+        } else {
+            if (isInside(secondRange.to)) {
+                intersectionRange.from = from;
+                intersectionRange.to = secondRange.to;
+            }
+            if (to < secondRange.to) {
+                intersectionRange.from = from;
+                intersectionRange.to = to;
+            }
+        }
+        return intersectionRange;
+    }
+// todo объединение в оптимизации и проверке
+    public Range[] toUnite(Range secondRange) {
+        Range[] unionRange = {null};
+        if (isInside(secondRange.from) || isInside(secondRange.to)) {
+            Range element = new Range(0, 0);
+            if (from <= secondRange.from) {
+                element.setFrom(from);
+            } else {
+                element.setFrom(secondRange.from);
+            }
+            if (to > secondRange.to) {
+                element.setTo(to);
+            } else {
+                element.setTo(secondRange.to);
+            }
+            unionRange[0] = element;
+        } else {
+            unionRange[0] = this;
+            unionRange[1] = secondRange;
+        }
+        return unionRange;
+    }
+    */
+
     public void setTo(double to) {
-        double max = (from > to) ? from : to;
-        double min = (from < to) ? from : to;
-        this.from = min;
-        this.to = max;
+        this.to = to;
     }
 
     public double getTo() {
-        return this.to;
+        return to;
     }
 
     public boolean isInside(double number) {
         return (from <= number && number <= to);
-
     }
 
     public double getLength() {
